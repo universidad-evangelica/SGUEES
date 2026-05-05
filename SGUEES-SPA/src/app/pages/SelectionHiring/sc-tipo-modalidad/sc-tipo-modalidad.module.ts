f0513ffb9357b36b2c29dce1572113ce6fa5f4f2
Emerson-Torres-UEES <emerson.torres@uees.edu.sv>
2026-05-05 11:45:06 -0600

    eliminado porque no sirve

--- SGUEES-SPA/src/app/pages/SelectionHiring/sc-tipo-modalidad/sc-tipo-modalidad.module.ts
+++
@@ -1,7 +0,0 @@
-import { NgModule } from '@angular/core';
-import { ScTipoModalidadRoutingModule } from './sc-tipo-modalidad-routing.module';
-
-@NgModule({
-	imports: [ScTipoModalidadRoutingModule],
-})
-export class ScTipoModalidadModule {}
