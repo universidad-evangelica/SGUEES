b521c433f5ad240d36a41d1fd13ae89cdbfa0445
Emerson-Torres-UEES <emerson.torres@uees.edu.sv>
2026-05-08 09:20:50 -0600

    GEN PAIS FINALIZADO

--- SGUEES-API/sguees.api/Options/General/GEN_PAIS/GEN_PAISService.cs
+++ SGUEES-API/sguees.api/Options/General/GEN_PAIS/GEN_PAISService.cs
@@ -19,6 +19,7 @@ namespace sguees.Services
 		{
 			var p = new List<CParameter>
 			{
+				new CParameter() {ParameterName="CORR_EMPRESA",Value=xWhere.CORR_EMPRESA,DbType=System.Data.DbType.Int32},
 			};
 			
 			return await _repo.GetAllAsync(p);
@@ -28,7 +29,8 @@ namespace sguees.Services
 		{
 			var p = new List<CParameter>
 			{
-				new CParameter() {ParameterName="CODIGO_PAIS",Value=xWhere.CODIGO_PAIS,DbType=System.Data.DbType.String},
+				new CParameter() {ParameterName="CORR_EMPRESA",Value=xWhere.CORR_EMPRESA,DbType=System.Data.DbType.Int32},
+				new CParameter() {ParameterName="CORR_PAIS",Value=xWhere.CORR_PAIS,DbType=System.Data.DbType.Int32},
 			};
 		
 			return await _repo.GetAsync(p);
---
+++ SGUEES-SPA/src/app/pages/General/gen-pais/models/gen-pais.ts
@@ -0,0 +1,14 @@
+export interface GenPais {
+	CORR_EMPRESA: number;
+	CORR_PAIS: number;
+	NOMBRE_PAIS: string;
+	CODIGO_PAIS: string;
+	NACIONALIDAD: string;
+	NOMBRE_CORTO: string;
+	USUARIO_CREA: string;
+	ESTACION_CREA: string;
+	FECHA_CREA: Date;
+	USUARIO_ACTU: string;
+	ESTACION_ACTU: string;
+	FECHA_ACTU: Date;
+}
