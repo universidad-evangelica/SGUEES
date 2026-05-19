8c634df2317d07628be3a89bf97e8fa6160907ec
Emerson-Torres-UEES <emerson.torres@uees.edu.sv>
2026-05-19 09:57:02 -0600

    SGUEES#25
    municipios complete

--- SGUEES-API/sguees.api/Options/General/GEN_MUNICIPIO/Models/GEN_MUNICIPIOTable.cs
+++ SGUEES-API/sguees.api/Options/General/GEN_MUNICIPIO/Models/GEN_MUNICIPIOTable.cs
@@ -5,8 +5,17 @@ namespace sguees.Models
 {
 	public class GEN_MUNICIPIOTable: BaseEntity
 	{
-		public string CODIGO_DEPTO { get; set; }
-		public string CODIGO_MUNICIPIO { get; set; }
+		public int CORR_EMPRESA { get; set; }
+		public int CORR_PAIS { get; set; }
+		public int CORR_DEPTO { get; set; }
+		public int CORR_MUNICIPIO { get; set; }
 		public string NOMBRE_MUNICIPIO { get; set; }
+		public string CODIGO_MUNICIPIO { get; set; }
+		public string USUARIO_CREA { get; set; }
+		public string ESTACION_CREA { get; set; }
+		public DateTime FECHA_CREA { get; set; }
+		public string USUARIO_ACTU { get; set; }
+		public string ESTACION_ACTU { get; set; }
+		public DateTime FECHA_ACTU { get; set; }
 	}
 }
---
+++ SGUEES-SPA/src/app/pages/General/gen-municipio/models/gen-municipio.ts
@@ -0,0 +1,16 @@
+export interface GenMunicipio {
+	CORR_EMPRESA: number;
+	CORR_PAIS: number;
+	CORR_DEPTO: number;
+	CORR_MUNICIPIO: number;
+	NOMBRE_MUNICIPIO: string;
+	CODIGO_MUNICIPIO: string;
+	USUARIO_CREA: string;
+	ESTACION_CREA: string;
+	FECHA_CREA: Date;
+	USUARIO_ACTU: string;
+	ESTACION_ACTU: string;
+	FECHA_ACTU: Date;
+	NOMBRE_DEPTO: string;
+	NOMBRE_PAIS: string;
+}
