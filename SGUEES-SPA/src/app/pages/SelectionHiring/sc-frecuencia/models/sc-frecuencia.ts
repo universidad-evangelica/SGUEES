eeb9d047a68c1fef9222415bc5a3d6ddf0a0f213
jjavalos95 <147444059+jjavalos95@users.noreply.github.com>
2026-06-18 08:31:45 -0600

    Sguees #38 (#46)
    * MTTO BASE PARA LA TABLA SC_INDUCCION
    
    SGUEES #35
    
    * Agregar Paginador al data-grid-mtto
    
    SGUEES #35.1, Solo es para que podamos implementar paginador a tablas transaccionales a futuro
    
    * MTTO BASE PARA LA TABLA SC_DESCRIPTOR_RESPONSABILIDAD_CARGO
    
    SGUEES #36
    
    * MTTO BASE PARA LA TABLA SC_DESCRIPTOR_RIESGO_PUESTO
    
    SGUEES #37
    
    * MTTO BASE PARA LA TABLA SC_FRECUENCIA
    
    SGUEES #38
    
    ---------
    
    Co-authored-by: JuanLuisHercules <juan.hernandez@uees.edu.sv>

---
+++ SGUEES-SPA/src/app/pages/SelectionHiring/sc-frecuencia/models/sc-frecuencia.ts
@@ -0,0 +1,12 @@
+export interface ScFrecuencia {
+	CORR_EMPRESA: number;
+	CORR_FRECUENCIA: number;
+	NOMBRE_FRECUENCIA: string;
+	ESTADO_FRECUENCIA: boolean;
+	USUARIO_CREA: string;
+	ESTACION_CREA: string;
+	FECHA_CREA: Date;
+	USUARIO_ACTU: string;
+	ESTACION_ACTU: string;
+	FECHA_ACTU: Date;
+}
