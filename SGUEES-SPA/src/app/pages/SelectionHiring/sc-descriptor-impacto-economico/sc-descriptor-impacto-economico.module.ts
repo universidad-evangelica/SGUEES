70553d22f241fb6582006ae934706b130d5f2c80
JuanLuisHercules <juan.hernandez@uees.edu.sv>
2026-06-18 13:30:44 -0600

    MTTO BASE PARA LA TABLA SC_DESCRIPTOR_IMPACTO_ECONOMICO
    SGUEES #58

---
+++ SGUEES-SPA/src/app/pages/SelectionHiring/sc-descriptor-impacto-economico/sc-descriptor-impacto-economico.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { ScDescriptorImpactoEconomicoRoutingModule } from './sc-descriptor-impacto-economico-routing.module';
+
+@NgModule({
+	imports: [ScDescriptorImpactoEconomicoRoutingModule],
+})
+export class ScDescriptorImpactoEconomicoModule {}
