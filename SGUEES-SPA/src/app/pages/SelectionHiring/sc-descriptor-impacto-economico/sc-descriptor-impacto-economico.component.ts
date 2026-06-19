14453f8ad3047f5580e9242a1cd5b2443916e075
JuanLuisHercules <juan.hernandez@uees.edu.sv>
2026-06-19 08:31:01 -0600

    AGREGAR MODO LECTURA AL MTTO SC_DESCRIPTOR_IMPACTO_ECONOMICO
    SGUEES #58.1, Se le agrego un modo lectura al MTTO de SC_DESCRIPTOR_IMPACTO_ECONOMICO cuando el usuario de doble clic sobre un registro de la tabla lo manda al usuario a una vista donde solo puede ver la información.

--- SGUEES-SPA/src/app/pages/SelectionHiring/sc-descriptor-impacto-economico/sc-descriptor-impacto-economico.component.ts
+++ SGUEES-SPA/src/app/pages/SelectionHiring/sc-descriptor-impacto-economico/sc-descriptor-impacto-economico.component.ts
@@ -52,6 +52,22 @@ export class ScDescriptorImpactoEconomicoComponent extends CBaseComponent implem
 		}
 	}
 
+	override rowDblClick(e: any): void {
+		const rowData = e?.data ?? e?.row?.data;
+		if (rowData) {
+			this.model = this.fillData(rowData);
+			this.modelUpdate = this.fillData(rowData);
+		}
+		super.rowDblClick(e);
+		setTimeout(() => {
+			if (!this.dataForm?.instance) {
+				return;
+			}
+			this.dataForm.instance.option('formData', this.model);
+			this.bloquear();
+		});
+	}
+
 	onEditClick(e: any): void {
 		if (!e?.row?.data) {
 			return;
