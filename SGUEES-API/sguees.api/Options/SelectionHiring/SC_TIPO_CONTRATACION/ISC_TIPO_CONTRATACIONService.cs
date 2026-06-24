50ad3b9232a50d8dcf47eecb2608b45adf1bd4df
LuisUees <luis.galdamez@uees.edu.sv>
2026-06-24 13:22:42 -0600

    Sguees#57.2 (#83)
    * Rediseño general aplicativo SGUEES#31
    
    Se rediseño el home y los layouts principales con styles y scss a una version mas reciente de Devextreme, manteniendo Angular 16
    
    * Modificacion diseño Login (frontend)
    
    * Correccion logo UEES en Login
    
    * Correccion mergin no push
    
    Corrijo el mergin pero solo local, sin push.
    
    * MTTO Tabla SC_TIPO_MODALIDAD
    
    Desarrollo mtto a la tabla sc_tipo_modalidad
    
    * MTTO TABLA SC_TIPO_CONTRATACION
    
    * Correccion de activacion/inactivacion item
    
    Se tocó el item padre para crear una variable customButtons, asi poder heredar a los hijos, para poder insertar aparte de las options "editar" "eliminar" de manera heredada, respetando la estructura.
    
    * Correccion de tabla SC_TIPO_CONTRATACION
    
    ---------
    
    Co-authored-by: jjavalos95 <147444059+jjavalos95@users.noreply.github.com>

--- SGUEES-API/sguees.api/Options/SelectionHiring/SC_TIPO_CONTRATACION/ISC_TIPO_CONTRATACIONService.cs
+++ SGUEES-API/sguees.api/Options/SelectionHiring/SC_TIPO_CONTRATACION/ISC_TIPO_CONTRATACIONService.cs
@@ -12,7 +12,7 @@ namespace SGUEES.Services
 		Task<CResult> UpdateAsync(SC_TIPO_CONTRATACIONTable Data, string vLOGIN_SISTEMA, string vESTACION);
 		Task<CResult> DeleteAsync(SC_TIPO_CONTRATACIONTable Data, string vLOGIN_SISTEMA, string vESTACION);
         //funcion para inactivar un registro, se actualiza el campo ACTIVO a false
-    Task<CResult> DesactivateAsync(SC_TIPO_CONTRATACIONTable Data, string vLOGIN_SISTEMA, string vESTACION);
+        Task<CResult> DesactivateAsync(SC_TIPO_CONTRATACIONTable Data, string vLOGIN_SISTEMA, string vESTACION);
 		//funcion para reactivar un registro, se actualiza el campo ACTIVO a true
 		Task<CResult> ReactivateAsync(SC_TIPO_CONTRATACIONTable Data, string vLOGIN_SISTEMA, string vESTACION);
     }
