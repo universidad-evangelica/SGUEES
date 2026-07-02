02420efbfeb10749f701f4c7bb9d3338b8a75774
JuanLuisHercules <juan.hernandez@uees.edu.sv>
2026-07-02 08:34:48 -0600

    ELIMINACIÓN DE VISTAS QUE NO SE OCUPARAN MAS
    Se eliminaron estas vistas pla-departamento, gen-pais, gen-municipio, gen-depto, gen-distrito, debido a que ya existe la vista estructura-territorial que es la que sirve para crear paises, departamentos, municipios, distritos y se hicieron unas correcciones a la vista de competencias-tecnicas

--- SGUEES-SPA/src/app/pages/Payroll/pla-departamento/pla-departamento.module.ts
+++
@@ -1,7 +0,0 @@
-import { NgModule } from '@angular/core';
-import { PlaDepartamentoRoutingModule } from './pla-departamento-routing.module';
-
-@NgModule({
-	imports: [PlaDepartamentoRoutingModule],
-})
-export class PlaDepartamentoModule {}
