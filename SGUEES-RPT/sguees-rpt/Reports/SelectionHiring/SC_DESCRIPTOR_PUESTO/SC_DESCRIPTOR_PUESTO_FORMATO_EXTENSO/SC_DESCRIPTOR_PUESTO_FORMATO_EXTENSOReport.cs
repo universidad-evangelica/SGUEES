e1ad1623434fd8dbc4b5d987c71631074fe3d1dd
JuanLuisHercules <juan.hernandez@uees.edu.sv>
2026-09-04 11:52:45 -0600

    Implementacion de funciones y actividades en el descriptor de puesto
    Implementacion de funciones y actividades en el descriptor de puesto

--- SGUEES-RPT/sguees-rpt/Reports/SelectionHiring/SC_DESCRIPTOR_PUESTO/SC_DESCRIPTOR_PUESTO_FORMATO_EXTENSO/SC_DESCRIPTOR_PUESTO_FORMATO_EXTENSOReport.cs
+++ SGUEES-RPT/sguees-rpt/Reports/SelectionHiring/SC_DESCRIPTOR_PUESTO/SC_DESCRIPTOR_PUESTO_FORMATO_EXTENSO/SC_DESCRIPTOR_PUESTO_FORMATO_EXTENSOReport.cs
@@ -75,7 +75,7 @@ namespace sgueesRpt.Reports.SelectionHiring.SC_DESCRIPTOR_PUESTO.SC_DESCRIPTOR_P
         
         [Browsable(false)]
         [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
-        public CrystalDecisions.CrystalReports.Engine.Section DetailSection1 {
+        public CrystalDecisions.CrystalReports.Engine.Section PageHeaderSectionFuncionesActividades {
             get {
                 return this.ReportDefinition.Sections[3];
             }
@@ -83,7 +83,7 @@ namespace sgueesRpt.Reports.SelectionHiring.SC_DESCRIPTOR_PUESTO.SC_DESCRIPTOR_P
         
         [Browsable(false)]
         [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
-        public CrystalDecisions.CrystalReports.Engine.Section ReportFooterSection1 {
+        public CrystalDecisions.CrystalReports.Engine.Section DetailSection1 {
             get {
                 return this.ReportDefinition.Sections[4];
             }
@@ -91,9 +91,17 @@ namespace sgueesRpt.Reports.SelectionHiring.SC_DESCRIPTOR_PUESTO.SC_DESCRIPTOR_P
         
         [Browsable(false)]
         [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
-        public CrystalDecisions.CrystalReports.Engine.Section PageFooterSection1 {
+        public CrystalDecisions.CrystalReports.Engine.Section ReportFooterSection1 {
             get {
                 return this.ReportDefinition.Sections[5];
+            }
+        }
+        
+        [Browsable(false)]
+        [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
+        public CrystalDecisions.CrystalReports.Engine.Section PageFooterSection1 {
+            get {
+                return this.ReportDefinition.Sections[6];
             }
         }
     }
